5ecd1766d71cdfe37f7b02f1f397f10673162724
Giovani Bossoni <gbossoni@mppr.mp.br>
2019-12-03 10:20:07 -0300

    Adcionado mais funcoes e atualizando git ignore

--- funcoes.py
+++ funcoes.py
@@ -59,3 +59,374 @@ def DataBase_creator(archivezip, nwigth, nheight, save_name):
 
     # Print the time-usage.
     print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
+
+
+def matrix_Bin(labels):
+    labels_bin=np.array([])
+
+    labels_name, labels0 = np.unique(labels, return_inverse=True)
+    labels0
+    
+    for _, i in enumerate(itemfreq(labels0)[:,0].astype(int)):
+        labels_bin0 = np.where(labels0 == itemfreq(labels0)[:,0][i], 1., 0.)
+        labels_bin0 = labels_bin0.reshape(1,labels_bin0.shape[0])
+
+        if (labels_bin.shape[0] == 0):
+            labels_bin = labels_bin0
+        else:
+            labels_bin = np.concatenate((labels_bin,labels_bin0 ),axis=0)
+
+    print("Nber SubVariables {0}".format(itemfreq(labels0)[:,0].shape[0]))
+    labels_bin = labels_bin.transpose()
+    print("Shape : {0}".format(labels_bin.shape))
+    
+    return labels_name, labels_bin
+
+
+def new_weights(shape):
+    return tf.Variable(tf.truncated_normal(shape, stddev=0.05))
+#outputs random value from a truncated normal distribution
+
+def new_biases(length):
+    return tf.Variable(tf.constant(0.05, shape=[length]))
+#outputs the constant value 0.05
+
+#%% [markdown]
+# **I mainly reuse the code of Hvass and his comments. **If you need more information, just visit his github (I put the link on the top of this kernel). His works and sharing are really great, I strongly advise you to have a look.
+# The main difference is that I added dropout.
+
+#%%
+def new_conv_layer(input,              # The previous layer.
+                   num_input_channels, # Num. channels in prev. layer.
+                   filter_size,        # Width and height of each filter.
+                   num_filters,        # Number of filters.
+                   use_pooling=True,
+                   use_dropout=True):  # Use 2x2 max-pooling.
+
+    # Shape of the filter-weights for the convolution.
+    # This format is determined by the TensorFlow API.
+    shape = [filter_size, filter_size, num_input_channels, num_filters]
+
+    # Create new weights aka. filters with the given shape.
+    weights = new_weights(shape=shape)
+
+    # Create new biases, one for each filter.
+    biases = new_biases(length=num_filters)
+
+    # Create the TensorFlow operation for convolution.
+    # Note the strides are set to 1 in all dimensions.
+    # The first and last stride must always be 1,
+    # because the first is for the image-number and
+    # the last is for the input-channel.
+    # But e.g. strides=[1, 2, 2, 1] would mean that the filter
+    # is moved 2 pixels across the x- and y-axis of the image.
+    # The padding is set to 'SAME' which means the input image
+    # is padded with zeroes so the size of the output is the same.
+    layer = tf.nn.conv2d(input=input,
+                         filter=weights,
+                         strides=[1, 1, 1, 1],
+                         padding='SAME')
+
+    # Add the biases to the results of the convolution.
+    # A bias-value is added to each filter-channel.
+    layer += biases
+
+    # Use pooling to down-sample the image resolution?
+    if use_pooling:
+        # This is 2x2 max-pooling, which means that we
+        # consider 2x2 windows and select the largest value
+        # in each window. Then we move 2 pixels to the next window.
+        layer = tf.nn.max_pool(value=layer,
+                               ksize=[1, 2, 2, 1],
+                               strides=[1, 2, 2, 1],
+                               padding='SAME')
+
+    # Rectified Linear Unit (ReLU).
+    # It calculates max(x, 0) for each input pixel x.
+    # This adds some non-linearity to the formula and allows us
+    # to learn more complicated functions.
+    layer = tf.nn.relu(layer)
+    
+    if use_dropout:
+        layer = tf.nn.dropout(layer,keep_prob_conv)
+
+    # Note that ReLU is normally executed before the pooling,
+    # but since relu(max_pool(x)) == max_pool(relu(x)) we can
+    # save 75% of the relu-operations by max-pooling first.
+
+    # We return both the resulting layer and the filter-weights
+    # because we will plot the weights later.
+    return layer, weights
+
+
+#%%
+def flatten_layer(layer):
+    # Get the shape of the input layer.
+    layer_shape = layer.get_shape()
+
+    # The shape of the input layer is assumed to be:
+    # layer_shape == [num_images, img_height, img_width, num_channels]
+
+    # The number of features is: img_height * img_width * num_channels
+    # We can use a function from TensorFlow to calculate this.
+    num_features = layer_shape[1:4].num_elements()
+    
+    # Reshape the layer to [num_images, num_features].
+    # Note that we just set the size of the second dimension
+    # to num_features and the size of the first dimension to -1
+    # which means the size in that dimension is calculated
+    # so the total size of the tensor is unchanged from the reshaping.
+    layer_flat = tf.reshape(layer, [-1, num_features])
+
+    # The shape of the flattened layer is now:
+    # [num_images, img_height * img_width * num_channels]
+
+    # Return both the flattened layer and the number of features.
+    return layer_flat, num_features
+
+
+#%%
+def new_fc_layer(input,          # The previous layer.
+                 num_inputs,     # Num. inputs from prev. layer.
+                 num_outputs,    # Num. outputs.
+                 use_relu=True,
+                 use_dropout=True): # Use Rectified Linear Unit (ReLU)?
+
+    # Create new weights and biases.
+    weights = new_weights(shape=[num_inputs, num_outputs])
+    biases = new_biases(length=num_outputs)
+
+    # Calculate the layer as the matrix multiplication of
+    # the input and weights, and then add the bias-values.
+    layer = tf.matmul(input, weights) + biases
+
+    # Use ReLU?
+    if use_relu:
+        layer = tf.nn.relu(layer)
+    
+    if use_dropout:
+        layer = tf.nn.dropout(layer,keep_prob_fc)
+        
+    return layer
+
+
+
+
+def main_breeds(labels_raw, Nber_breeds , all_breeds='TRUE'):
+
+    #%%
+batch_size = 50
+
+#function next_batch
+def next_batch(num, data, labels):
+    '''
+    Return a total of `num` random samples and labels. 
+    '''
+    idx = np.arange(0 , len(data))
+    np.random.shuffle(idx)
+    idx = idx[:num]
+    data_shuffle = [data[i] for i in idx]
+    labels_shuffle = [labels[i] for i in idx]
+
+    return np.asarray(data_shuffle), np.asarray(labels_shuffle)
+
+
+    labels_freq_pd = itemfreq(labels_raw["breed"])
+    labels_freq_pd = labels_freq_pd[labels_freq_pd[:, 1].argsort()[::-1]] #[::-1] ==> to sort in descending order
+    
+    if all_breeds == 'FALSE':
+        main_labels = labels_freq_pd[:,0][0:Nber_breeds]
+    else: 
+        main_labels = labels_freq_pd[:,0][:]
+        
+    labels_raw_np = labels_raw["breed"].as_matrix() #transform in numpy
+    labels_raw_np = labels_raw_np.reshape(labels_raw_np.shape[0],1)
+
+    labels_filtered_index = np.where(labels_raw_np == main_labels)
+    
+    return labels_filtered_index
+
+
+def optimize(num_iterations, X):
+    global total_iterations
+    
+    start_time = time.time()
+    
+    #array to plot
+    losses = {'train':[], 'validation':[]}
+    
+    for i in range(num_iterations):
+            total_iterations += 1
+            # Get a batch of training examples.
+            # x_batch now holds a batch of images and
+            # y_true_batch are the true labels for those images.
+            x_batch, y_true_batch = next_batch(batch_size, X_train, y_train)
+
+            # Put the batch into a dict with the proper names
+            # for placeholder variables in the TensorFlow graph.
+            feed_dict_train = {x: x_batch,
+                               y_true: y_true_batch,
+                               keep_prob_conv : 0.3,
+                               keep_prob_fc : 0.4}
+            feed_dict_validation = {x: X_validation,
+                               y_true: y_validation,
+                               keep_prob_conv : 1,
+                               keep_prob_fc : 1}
+            
+            # Run the optimizer using this batch of training data.
+            # TensorFlow assigns the variables in feed_dict_train
+            # to the placeholder variables and then runs the optimizer.
+            session.run(optimizer, feed_dict=feed_dict_train)
+            
+            acc_train = session.run(accuracy, feed_dict=feed_dict_train)
+            acc_validation = session.run(accuracy, feed_dict=feed_dict_validation)
+            losses['train'].append(acc_train)
+            losses['validation'].append(acc_validation)
+            
+            # Print status every X iterations.
+            if (total_iterations % X == 0) or (i ==(num_iterations -1)):
+            # Calculate the accuracy on the training-set.
+                                
+                msg = "Iteration: {0:>6}, Training Accuracy: {1:>6.1%}, Validation Accuracy: {2:>6.1%}"
+                print(msg.format(total_iterations, acc_train, acc_validation))
+                
+    
+    # Ending time.
+    end_time = time.time()
+
+    # Difference between start and end-times.
+    time_dif = end_time - start_time
+
+    # Print the time-usage.
+    print("Time usage: " + str(timedelta(seconds=int(round(time_dif)))))
+    
+    plt.plot(losses['train'], label='Training loss')
+    plt.plot(losses['validation'], label='Validation loss')
+    plt.legend()
+    _ = plt.ylim()
+
+
+def plot_images(images, cls_true, cls_pred=None):
+    assert len(images) == len(cls_true) == 12
+    
+    # Create figure with 3x3 sub-plots.
+    fig, axes = plt.subplots(4, 3)
+    fig.subplots_adjust(hspace=0.3, wspace=0.3)
+
+    for i, ax in enumerate(axes.flat):
+        # Plot image.
+        ax.imshow(images[i].reshape(img_shape), cmap='binary')
+
+        # Show true and predicted classes.
+        if cls_pred is None:
+            xlabel = "True: {0}".format(cls_true[i])
+        else:
+            xlabel = "True: {0}, Pred: {1}".format(cls_true[i], cls_pred[i])
+
+        # Show the classes as the label on the x-axis.
+        ax.set_xlabel(xlabel)
+        
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+
+#%%
+def plot_confusion_matrix(data_pred_cls,data_predicted_cls):
+    # This is called from print_test_accuracy() below.
+
+    # cls_pred is an array of the predicted class-number for
+    # all images in the test-set.
+  
+    # Get the confusion matrix using sklearn.
+    cm = confusion_matrix(y_true=data_pred_cls,
+                          y_pred=data_predicted_cls)
+
+    # Print the confusion matrix as text.
+    print(cm)
+
+    # Plot the confusion matrix as an image.
+    plt.matshow(cm)
+
+    # Make various adjustments to the plot.
+    plt.colorbar()
+    tick_marks = np.arange(num_classes)
+    plt.xticks(tick_marks, range(num_classes))
+    plt.yticks(tick_marks, range(num_classes))
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+
+    # Ensure the plot is shown correctly with multiple plots
+    # in a single Notebook cell.
+    plt.show()
+
+
+def plot_conv_layer(layer, image):
+    feed_dict = {x: [image],
+                keep_prob_conv : 1,
+                keep_prob_fc : 1}
+
+    values = session.run(layer, feed_dict=feed_dict)
+
+    num_filters = values.shape[3]
+
+    # Number of grids to plot.
+    # Rounded-up, square-root of the number of filters.
+    num_grids = math.ceil(math.sqrt(num_filters))
+    
+    # Create figure with a grid of sub-plots.
+    fig, axes = plt.subplots(num_grids, num_grids)
+
+    # Plot the output images of all the filters.
+    for i, ax in enumerate(axes.flat):
+        # Only plot the images for valid filters.
+        if i<num_filters:
+            img = values[0, :, :, i]
+
+            # Plot image.
+            ax.imshow(img, interpolation='nearest', cmap='binary')
+        
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    plt.show()
+
+
+#%%
+def plot_conv_weights(weights, input_channel):
+
+    w = session.run(weights)
+
+    w_min = np.min(w)
+    w_max = np.max(w)
+
+    # Number of filters used in the conv. layer.
+    num_filters = w.shape[3]
+
+    # Number of grids to plot.
+    # Rounded-up, square-root of the number of filters.
+    num_grids = math.ceil(math.sqrt(num_filters))
+    
+    # Create figure with a grid of sub-plots.
+    fig, axes = plt.subplots(num_grids, num_grids)
+
+    # Plot all the filter-weights.
+    for i, ax in enumerate(axes.flat):
+        # Only plot the valid filter-weights.
+        if i<num_filters:
+            img = w[:, :, input_channel, i]
+
+            # Plot image.
+            ax.imshow(img, vmin=w_min, vmax=w_max,
+                      interpolation='nearest', cmap='seismic')
+        
+        # Remove ticks from the plot.
+        ax.set_xticks([])
+        ax.set_yticks([])
+    
+    plt.show()
--- teste_giovani.py
+++ teste_giovani.py
@@ -26,6 +26,8 @@ import pandas as pd
 from zipfile import ZipFile
 from io import BytesIO
 
+import funcoes
+
 # check tensorflow version
 tf.__version__
 
@@ -34,11 +36,55 @@ PREFIXO = './'
 
 # %%
 # We unzip the train and test zip file
-archive_train = ZipFile("./projeto_final_rpi/Data/train.zip", 'r')
-archive_test = ZipFile("./projeto_final_rpi/Data/test.zip", 'r')
+archive_train = ZipFile(PREFIXO + "/Data/train.zip", 'r')
+archive_test = ZipFile(PREFIXO + "/Data/test.zip", 'r')
 
-# This line shows the 5 first image name of the train database
-archive_train.namelist()[0:5]
+# %%
+# **3. Resize and normalize the data**
+print("normalizando os dados e resize")
+image_resize = 60
+funcoes.DataBase_creator(archivezip = archive_train, nwigth = image_resize, nheight = image_resize , save_name = "train")
+funcoes.DataBase_creator(archivezip = archive_test, nwigth = image_resize, nheight = image_resize , save_name = "test")
 
-# This line shows the number of images in the train database
-len(archive_train.namelist()[:])-1  # we must remove the 1st value
+
+#%%
+#load TRAIN
+print('carregando train e test')
+train = pickle.load( open( "train.p", "rb" ) )
+print('shape train: ' + str(train.shape))
+test = pickle.load( open( "test.p", "rb" ) )
+print('shape test: ' + str(test.shape))
+
+
+# %%
+#%%
+print('load csv')
+df_train= pd.read_csv(PREFIXO+'/Data/labels.csv')
+df_train.sample(5)
+#%%
+#######Upload the zip (input data base)########
+labels_raw = pd.read_csv(PREFIXO+'/Data/labels.csv', header=0, sep=',', quotechar='"')
+
+#Check 5 random values
+labels_raw.sample(5)
+# %%
+Nber_of_breeds = 8
+
+labels_filtered_index = funcoes.main_breeds(labels_raw = labels_raw, Nber_breeds = Nber_of_breeds, all_breeds='FALSE')
+labels_filtered = labels_raw.iloc[labels_filtered_index[0],:]
+train_filtered = train[labels_filtered_index[0],:,:,:]
+
+print('- Number of images remaining after selecting the {0} main breeds : {1}'.format(Nber_of_breeds, labels_filtered_index[0].shape))
+print('- The shape of train_filtered dataset is : {0}'.format(train_filtered.shape))
+#%%
+#print(labels_filtered[90])
+lum_img = train_filtered[1,:,:,:]
+plt.imshow(lum_img)
+plt.show()
+
+# %%
+#%%
+#We select the labels from the N main breeds
+labels = labels_filtered["breed"].as_matrix()
+labels = labels.reshape(labels.shape[0],1) #labels.shape[0] looks faster than using len(labels)
+labels.shape
